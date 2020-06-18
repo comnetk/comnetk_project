f6012f0dac32801e71d5fe4bc8a350b28630af58
권동환 <dhkwon@forbiz.co.kr>
2020-06-18 12:54:18 +0900

    fixup! intellij에서 커밋

--- src/test/java/com/comnetk/web/HelloControllerTest.java
+++ src/test/java/com/comnetk/web/HelloControllerTest.java
@@ -10,7 +10,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -26,6 +28,21 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+
+    }
+
+    @Test
+    public void hellDto_return() throws Exception{
+        String name = "dhkwon";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                    .param("name", name)
+                    .param("amount", String.valueOf(amount)))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name", is(name)))
+                    .andExpect(jsonPath("$.amount",is(amount)));
 
     }
 }
